1fc924246b60c1784466c570dfe0b6ac742a9e85
Álvaro Justen aka Turicas <alvaro@justen.eng.br>
2011-11-23 16:24:27 -0200

    Fixed little bug in create-symlinks.sh

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -3,11 +3,14 @@
 path=$(pwd)
 this=$(basename $0)
 for file in $(ls -a); do
-    if [ "$file" != "$this" ] && [ "$file" != "." ] && [ "$file" != ".." ]; then
-        if [ ! -e ~/"$file" ]; then
+    if [ "$file" != "$this" ] && [ "$file" != "." ] && [ "$file" != ".." ] && \
+       [ "$file" != ".git" ]; then
+        if [ ! -e ~/$file ]; then
             ln -s "$path/$file" ~/"$file"
         else
-            echo "*** Link to $file NOT created! File already exists."
+            echo "*** Link to '$file' NOT created! File already exists."
         fi
+    else
+        echo "*** Ignoring file '$file'"
     fi
 done
