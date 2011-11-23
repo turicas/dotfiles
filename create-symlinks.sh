8638a3b83651149d74a4c66ab08a912d72eaf576
Álvaro Justen <alvaro@ideas.(none)>
2011-11-23 16:21:09 -0200

    Changes made by SparkleShare

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -5,7 +5,7 @@ this=$(basename $0)
 for file in $(ls -a); do
     if [ "$file" != "$this" ] && [ "$file" != "." ] && [ "$file" != ".." ]; then
         if [ ! -e ~/$file ]; then
-            ln -s "$path/$file" ~/"$file"
+            ln -s "$path/$file" ~/$file
         else
             echo "*** Link to $file NOT created! File already exists."
         fi
