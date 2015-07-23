2a9223974b8298214f113f7516316834b7aad54d
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2015-07-23 02:15:56 -0300

    Add a Python wrapper around i3status

---
+++ .i3/wrapper.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This script is a simple wrapper which prefixes each i3status line with custom
+# information. It is a python reimplementation of:
+# http://code.stapelberg.de/git/i3status/tree/contrib/wrapper.pl
+#
+# To use it, ensure your ~/.i3status.conf contains this line:
+#     output_format = "i3bar"
+# in the 'general' section.
+# Then, in your ~/.i3/config, use:
+#     status_command i3status | ~/i3status/contrib/wrapper.py
+# In the 'bar' section.
+#
+# In its current version it will display the cpu frequency governor, but you
+# are free to change it to display whatever you like, see the comment in the
+# source code below.
+#
+# © 2012 Valentin Haenel <valentin.haenel@gmx.de>
+#
+# This program is free software. It comes without any warranty, to the extent
+# permitted by applicable law. You can redistribute it and/or modify it under
+# the terms of the Do What The Fuck You Want To Public License (WTFPL), Version
+# 2, as published by Sam Hocevar. See http://sam.zoy.org/wtfpl/COPYING for more
+# details.
+
+import json
+import sys
+import urllib
+
+
+IPIFY_URL = 'http://api.ipify.org/'
+
+def get_public_ip():
+    'Get the current public IP address'
+    try:
+        url = urllib.urlopen(IPIFY_URL)
+    except:
+        return '(unknown IP)'
+    else:
+        return url.read().strip()
+
+def print_line(message):
+    """ Non-buffered printing to stdout. """
+    sys.stdout.write(message + '\n')
+    sys.stdout.flush()
+
+def read_line():
+    """ Interrupted respecting reader for stdin. """
+    # try reading a line, removing any extra whitespace
+    try:
+        line = sys.stdin.readline().strip()
+        # i3status sends EOF, or an empty line
+        if not line:
+            sys.exit(3)
+        return line
+    # exit on ctrl-c
+    except KeyboardInterrupt:
+        sys.exit()
+
+if __name__ == '__main__':
+    # Skip the first line which contains the version header.
+    print_line(read_line())
+
+    # The second line contains the start of the infinite array.
+    print_line(read_line())
+
+    while True:
+        line, prefix = read_line(), ''
+        # ignore comma at start of lines
+        if line.startswith(','):
+            line, prefix = line[1:], ','
+
+        j = json.loads(line)
+        data = {'name': 'public_ip',
+                'full_text': '{}'.format(get_public_ip()),}
+        wireless_index = [element['name'] for element in j].index('wireless')
+        j.insert(wireless_index, data)
+        # and echo back new encoded json
+        print_line(prefix + json.dumps(j))
