6f6da4baee32cc9fd058bb748c7eedbd8792f4f2
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2015-08-08 22:24:07 -0300

    Update needed packages

--- install.sh
+++ install.sh
@@ -18,14 +18,28 @@ aptitude -y install \
     chromium chromium-inspector \
     gimp inkscape
 
+apt-get -y install libmariadbd-dev libssl-dev libxslt-dev libxml2-dev \
+    htop iotop powertop lsof libpq-dev postgresql-client libffi-dev \
+    libtiff-dev libjpeg-dev libwebp-dev libopenjpeg-dev nodejs npm \
+    docker.io flashplugin-nonfree xchat
+# TODO: arduino, firefox nightly, googletalk-plugin, skype
+
+adduser turicas docker
+adduser turicas sudo
+npm install grunt grunt-cli bower marked
+
+if [ ! -e "/usr/bin/node" ]; then
+    ln -s /usr/bin/node{js,}
+fi
+
 aptitude -y remove apache2 exim4
 easy_install pip
-pip install virtualenv virtualenvwrapper tox py3status
+pip install virtualenv virtualenvwrapper httpie youtube-dl
 
 # URxvt stuff
 URXVT_PERL=/usr/lib/urxvt/perl/
-mkdir code
-cd code/
+mkdir projects
+cd projects/
 sudo -u turicas git clone https://github.com/muennich/urxvt-perls.git
 sudo -u turicas git clone https://github.com/majutsushi/urxvt-font-size.git
 cp urxvt-font-size/font-size $UXRVT_PERL
