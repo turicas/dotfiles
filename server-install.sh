dc841ef02263485370586a1213c8118441966d97
Álvaro Justen <alvarojusten@gmail.com>
2021-07-24 08:01:59 -0300

    Update server-install.sh

--- server-install.sh
+++ server-install.sh
@@ -12,19 +12,20 @@ apt -y install $(cat server-apt-packages.txt)
 # Python packages
 pip3 install -r server-pip-packages.txt
 
-# Docker
-# TODO: replace `buster` with current last release
+# Docker base install
 apt remove docker docker.io containerd runc
 curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian buster stable"
 apt update
 apt -y install docker-ce docker-ce-cli containerd.io
+
+# Pre-pull some docker images
 cat server-docker-images.txt | while read image; do
    docker pull $image
 done
 # TODO: Fix docker: echo '@{PROC}=/proc/' > /etc/apparmor.d/tunables/proc
 
-# Dokku
+# Dokku base install
 wget -nv -O - https://packagecloud.io/dokku/dokku/gpgkey | apt-key add -
 OS_ID="$(lsb_release -cs 2>/dev/null || echo "bionic")"
 echo "xenial bionic focal" | grep -q "$OS_ID" || OS_ID="bionic"
@@ -32,10 +33,14 @@ echo "deb https://packagecloud.io/dokku/dokku/ubuntu/ ${OS_ID} main" | sudo tee 
 sudo apt-get update -qq >/dev/null
 sudo apt-get install -qq -y dokku
 sudo dokku plugin:install-dependencies --core
+
+# Install Dokku plugins:
+dokku plugin:install https://github.com/dokku/dokku-maintenance.git maintenance
 dokku plugin:install https://github.com/dokku/dokku-postgres.git postgres
 dokku plugin:install https://github.com/dokku/dokku-letsencrypt.git
 dokku plugin:install https://github.com/dokku/dokku-mysql.git mysql
 dokku plugin:install https://github.com/dokku/dokku-redis.git redis
+
 # Optional - move dokku services storage to another partition:
 # mv /var/lib/dokku/services /mnt/data/dokku-services
 # mkdir /var/lib/dokku/services
