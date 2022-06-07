9b512dd739454a50046dbf9049c9df4461c9df15
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2022-06-07 02:15:04 -0300

    Fix Dropbox install

--- desktop-install.sh
+++ desktop-install.sh
@@ -14,22 +14,24 @@ apt clean
 mkdir -p /etc/apt/keyrings
 
 # Docker (via APT)
-apt remove docker docker-engine docker.io containerd runc
+apt remove docker docker.io containerd runc
 curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian bullseye stable" | sudo tee /etc/apt/sources.list.d/docker.list
 apt update
 apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 apt clean
 
-# Dropbox (via APT)
-echo "deb [arch=amd64] http://linux.dropbox.com/debian sid main" > /etc/apt/sources.list.d/dropbox.list
-apt update
-apt install -y dropbox
+# Dropbox (via DEB)
+apt install -y libpango1.0-0
 apt clean
+wget -O "/tmp/dropbox.deb" "https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2020.03.04_amd64.deb"
+dpkg -i "/tmp/dropbox.deb"
+rm "/tmp/dropbox.deb"
 
 # Google Chrome (via .deb)
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-dpkg -i google-chrome-stable_current_amd64.deb
+wget -O "/tmp/google-chrome.deb" https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -i "/tmp/google-chrome.deb"
+rm "/tmp/google-chrome.deb"
 
 # pip packages
 pip3 install -r desktop-pip-packages.txt
