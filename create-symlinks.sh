71b0cbc82406164f9dde45e87ee4550b4d2f015b
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2022-06-07 02:14:43 -0300

    Copy only dotfiles

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -1,39 +1,11 @@
 #!/bin/bash
 
 path=$(pwd)
-this=$(basename $0)
-do_not_copy=(
-   "$this"
-   "."
-   ".."
-   ".git"
-   ".gitignore"
-   "desktop-apt-packages.txt"
-   "desktop-install.sh"
-   "desktop-pip-packages.txt"
-   "desktop-post-install.sh"
-   "server-apt-packages.txt"
-   "server-docker-images.txt"
-   "server-install.sh"
-   "server-pip-packages.txt"
-)
-
-for file in .* * .config/*; do
-    allowed=yes
-    for not_allowed in ${do_not_copy[@]}; do
-        if [ "$not_allowed" = "$file" ]; then
-            allowed=no
-        fi
-    done
-
-    if [ $allowed = "no" ]; then
-        echo "*** Ignoring file '$file'"
+for file in .*; do
+    if [ ! -e "$HOME/$filename" ]; then
+        ln -s "$path/$filename" "$HOME/$filename"
+        echo "*** Created link to '$filename'"
     else
-        if [ ! -e "$HOME/$file" ]; then
-            ln -s "$path/$file" "$HOME/$file"
-            echo "*** Created link to '$file'"
-        else
-            echo "*** Link to '$file' NOT created! File already exists."
-        fi
+        echo "*** Link to '$filename' NOT created! File already exists."
     fi
 done
