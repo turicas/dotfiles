57ad204ce7818f379cec43f54d796793bf276435
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2024-03-30 08:46:43 -0300

    Add script to install custom fonts

---
+++ install-fonts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts
+
+for nerdfont in DejaVuSansMono.zip UbuntuMono.zip; do
+	wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/$nerdfont"
+	unzip -o "$nerdfont"
+	rm -f "$nerdfont" README.md LICENSE.txt
+done
+fc-cache -fv
+fc-list | grep -i "nerd"
