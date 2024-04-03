4fa70cec3f0ffbaebbf644fc8a0cc878043a92d5
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2024-04-03 12:39:34 -0300

    Add duckdb installation script

---
+++ install/duckdb.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+pushd ~/.local/bin
+duckdb_url=$(
+	wget --quiet -O - https://github.com/duckdb/duckdb/releases \
+	| grep -oE --color=no 'href="(/[^ ]+-linux-amd64.zip)"' \
+	| sed 's|href="|https://github.com|; s|"$||' \
+	| head -1
+)
+wget -O duckdb.zip "$duckdb_url"
+unzip duckdb.zip duckdb
+rm duckdb.zip
+chmod +x duckdb
+popd
--- install/tree-sitter.sh
+++ install/tree-sitter.sh
@@ -6,7 +6,8 @@ pushd ~/.local/bin
 treesitter_url=$(
 	wget --quiet -O - https://github.com/tree-sitter/tree-sitter/releases \
 	| grep -oE --color=no 'href="(/[^ ]+-linux-x64.gz)"' \
-	| sed 's|href="|https://github.com|; s|"$||'
+	| sed 's|href="|https://github.com|; s|"$||' \
+	| head -1
 )
 wget -O tree-sitter.gz "$treesitter_url"
 gunzip -f tree-sitter.gz
@@ -26,8 +27,10 @@ done
 popd
 
 # TODO: configure vim to generate tags on each save, like https://gist.github.com/romainl/f2d0727bdb9bde063531cd237f47775f
-# rm -rf .tags; touch .tags
+# rm -rf tags
 # git ls-files | while read filename; do
-# 	tree-sitter tags $filename | sed "s/\t/\t${filename}\t/" >> .tags
-# done
+# 	tree-sitter tags $filename 2> /dev/null \
+# 	| sed 's/| //; s/[dr]ef ([0-9]\+, [0-9]\+) - ([0-9]\+, [0-9]\+) `//; s/`$//' \
+# 	| awk -F '\t' '{print $1 "\t'$filename'\t/" $3 "/\t" $2}'
+# done > tags
 # TODO: may use https://github.com/npezza93/ttags/
