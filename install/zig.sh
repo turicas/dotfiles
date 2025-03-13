a5e86b0d5e4774eb48c69224890390842335782c
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2025-03-13 00:53:17 -0300

    Update Zig version to 0.14.0

--- install/zig.sh
+++ install/zig.sh
@@ -4,9 +4,10 @@ set -e
 
 mkdir -p ~/.local/opt
 cd ~/.local/opt
-wget https://ziglang.org/download/0.13.0/zig-linux-x86_64-0.13.0.tar.xz
-tar xfvJ zig-linux-x86_64-0.13.0.tar.xz
-rm zig-linux-x86_64-0.13.0.tar.xz
-rm -rf ~/.local/bin/zig
+VERSION="0.14.0"
+wget "https://ziglang.org/download/$VERSION/zig-linux-x86_64-$VERSION.tar.xz"
+tar xfvJ "zig-linux-x86_64-$VERSION.tar.xz"
+rm "zig-linux-x86_64-$VERSION.tar.xz"
 cd -
-ln -s ~/.local/opt/zig-*/zig ~/.local/bin/
+rm -f ~/.local/bin/zig
+ln -s ~/.local/opt/zig-*-$VERSION/zig ~/.local/bin/
