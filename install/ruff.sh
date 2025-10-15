2ad44e50c9f37f5d5cb3a1b8fb7ced970a2645f9
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2025-10-15 14:57:27 -0300

    Add script to install ruff from github

---
+++ install/ruff.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+pushd ~/.local/bin
+downloadUrl=$(
+	wget --quiet -O - https://github.com/astral-sh/ruff/releases \
+	| grep -oE --color=no 'href="([^ ]+-x86_64-unknown-linux-gnu.tar.gz)"' \
+	| sed 's|href="||; s|"$||' \
+	| head -1
+)
+downloadFile="ruff.tar.gz"
+wget -O "$downloadFile" "$downloadUrl"
+tar xfz "$downloadFile"
+mv ruff-x86_64-unknown-linux-gnu/ruff .
+rm "$downloadFile"
+rmdir "ruff-x86_64-unknown-linux-gnu"
+chmod +x ruff
+popd
