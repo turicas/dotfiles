14d20d14e4b32b9484f58bcbf499f03447e7b16d
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2025-12-09 00:46:49 -0300

    Update Zig version and install script

--- install/zig.sh
+++ install/zig.sh
@@ -4,10 +4,12 @@ set -e
 
 mkdir -p ~/.local/opt
 cd ~/.local/opt
-VERSION="0.14.0"
-wget "https://ziglang.org/download/$VERSION/zig-linux-x86_64-$VERSION.tar.xz"
-tar xfvJ "zig-linux-x86_64-$VERSION.tar.xz"
-rm "zig-linux-x86_64-$VERSION.tar.xz"
+VERSION="0.15.2"
+URL="https://ziglang.org/download/${VERSION}/zig-x86_64-linux-${VERSION}.tar.xz"
+FILENAME=$(basename "$URL")
+wget "$URL"
+tar xfvJ "$FILENAME"
+rm "$FILENAME"
 cd -
 rm -f ~/.local/bin/zig
 ln -s ~/.local/opt/zig-*-$VERSION/zig ~/.local/bin/
